4907a8bd69e0432b46502d82f6c1a010e24ab4ea
pedro.mendes <pedro.mendes@corp.globo.com>
2018-05-08 10:01:43 -0300

    main: remove code from main package

--- main.go
+++ main.go
@@ -1,70 +1,18 @@
 package main
 
 import (
-	"net/http"
 	"log"
-	"encoding/json"
 	"fmt"
+	"github.com/pedrolopesme/iss-notifier/iss"
 )
-
-// Full endpoint URL
-const openNotifyEndpoint = "http://api.open-notify.org/iss-now.json"
-
-// ISS Position
-type IssPosition struct {
-	Latitude  string `json:"latitude"`
-	Longitude string `json:"longitude"`
-}
-
-// OpenNotify Response
-// More at open-notify.org/Open-Notify-API/ISS-Location-Now/
-type OpenNotifyResponse struct {
-	Message     string      `json:"message"`
-	Timestamp   int         `json:"timestamp"`
-	IssPosition IssPosition `json:"iss_position"`
-}
-
-// getISSCoordinate get satellite coordinate relative to Earth
-func getISSCoordinate() (position IssPosition, err error) {
-
-	// Build the request to OpenNotify
-	req, err := http.NewRequest("GET", openNotifyEndpoint, nil)
-	if err != nil {
-		return
-	}
-
-	// Http Client
-	client := &http.Client{}
-
-	// Making the request
-	httpResponse, err := client.Do(req)
-	if err != nil {
-		return
-	}
-
-	// Closing Body when done reading
-	defer httpResponse.Body.Close()
-
-	// Fill the response from OpenNotify
-	var response OpenNotifyResponse
-
-	// Decoding the response
-	if err = json.NewDecoder(httpResponse.Body).Decode(&response); err != nil {
-		return
-	}
-
-	position = response.IssPosition
-	return
-
-}
-
 
 func main() {
 
 	// Retrieving coordinate
-	issCoordinate, err := getISSCoordinate()
+	issCoordinate, err := iss.GetCoordinate()
 	if err != nil {
-		log.Fatal("getISSCoordinate: ", err)
+		log.Fatal("iss.GetCoordinate: ", err)
 	}
 	fmt.Println(issCoordinate)
+
 }
