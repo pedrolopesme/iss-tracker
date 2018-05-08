d4f6e2029dbde3f1de739a12440d6105a85179b2
pedro.mendes <pedro.mendes@corp.globo.com>
2018-05-08 09:51:00 -0300

    iss/checker: move all iss code to its own package

---
+++ iss/checker.go
@@ -0,0 +1,57 @@
+package iss
+
+import (
+	"net/http"
+	"encoding/json"
+)
+
+// Full endpoint URL
+const openNotifyEndpoint = "http://api.open-notify.org/iss-now.json"
+
+// ISS Position
+type IssPosition struct {
+	Latitude  string `json:"latitude"`
+	Longitude string `json:"longitude"`
+}
+
+// OpenNotify Response
+// More at open-notify.org/Open-Notify-API/ISS-Location-Now/
+type OpenNotifyResponse struct {
+	Message     string      `json:"message"`
+	Timestamp   int         `json:"timestamp"`
+	IssPosition IssPosition `json:"iss_position"`
+}
+
+// GetCoordinate get satellite coordinate relative to Earth
+func GetCoordinate() (position IssPosition, err error) {
+
+	// Build the request to OpenNotify
+	req, err := http.NewRequest("GET", openNotifyEndpoint, nil)
+	if err != nil {
+		return
+	}
+
+	// Http Client
+	client := &http.Client{}
+
+	// Making the request
+	httpResponse, err := client.Do(req)
+	if err != nil {
+		return
+	}
+
+	// Closing Body when done reading
+	defer httpResponse.Body.Close()
+
+	// Fill the response from OpenNotify
+	var response OpenNotifyResponse
+
+	// Decoding the response
+	if err = json.NewDecoder(httpResponse.Body).Decode(&response); err != nil {
+		return
+	}
+
+	position = response.IssPosition
+	return
+
+}
