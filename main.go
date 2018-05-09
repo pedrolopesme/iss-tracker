daeae840f921e8f5a7852e42588f25cd95dbda59
Pedro Mendes <pedro.mendes@peixeurbano.com>
2018-05-08 23:03:23 -0300

    main: add AWS Lambda package

--- main.go
+++ main.go
@@ -4,15 +4,18 @@ import (
 	"log"
 	"fmt"
 	"github.com/pedrolopesme/iss-notifier/iss"
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func main() {
-
+func track() (string, error) {
 	// Retrieving coordinate
 	issCoordinate, err := iss.GetCoordinate()
 	if err != nil {
 		log.Fatal("iss.GetCoordinate: ", err)
 	}
-	fmt.Println(issCoordinate)
+	return fmt.Sprintf("%#v", issCoordinate), err
+}
 
+func main() {
+	lambda.Start(track)
 }
