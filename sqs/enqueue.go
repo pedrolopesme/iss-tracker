4c39ea82c23e21242e251c620635d2228453519e
pedro.mendes <pedro.mendes@corp.globo.com>
2018-05-15 10:00:18 -0300

    sqs/enqueue: move SQS handling logic to its own package

---
+++ sqs/enqueue.go
@@ -0,0 +1,42 @@
+package sqs
+
+import (
+	"github.com/pedrolopesme/iss-notifier/iss"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
+	"github.com/aws/aws-sdk-go/aws"
+	"fmt"
+)
+
+func Enqueue(queueUrl string, position iss.IssPosition) (messageId string, err error) {
+
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	svc := sqs.New(sess)
+
+	result, err := svc.SendMessage(&sqs.SendMessageInput{
+		DelaySeconds: aws.Int64(10),
+		MessageAttributes: map[string]*sqs.MessageAttributeValue{
+			"Latitude": &sqs.MessageAttributeValue{
+				DataType:    aws.String("String"),
+				StringValue: aws.String(position.Latitude),
+			},
+			"Longitude": &sqs.MessageAttributeValue{
+				DataType:    aws.String("String"),
+				StringValue: aws.String(position.Longitude),
+			},
+		},
+		MessageBody: aws.String("ISS Coordinate"),
+		QueueUrl:    &queueUrl,
+	})
+
+	if err != nil {
+		fmt.Println("Error", err)
+		return
+	}
+
+	messageId =  *result.MessageId
+	return
+}
